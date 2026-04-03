f893fe18d7177852bd90d8a4be181199a2cd0135
Garrett D'Amore <garrett@damore.org>
2026-04-02 22:14:26 -0700

    Improve help overlay styling

--- internal/view/help.go
+++ internal/view/help.go
@@ -5,8 +5,11 @@ package view
 
 import (
 	"github.com/gdamore/goless/internal/ansi"
+	"github.com/gdamore/goless/internal/model"
 	"github.com/gdamore/tcell/v3"
 )
+
+const helpDocChunkSize = 256
 
 func (v *Viewer) drawHelp(screen tcell.Screen) {
 	bodyStyle := v.toTCellStyle(ansi.DefaultStyle())
@@ -16,7 +19,7 @@ func (v *Viewer) drawHelp(screen tcell.Screen) {
 		screen.PutStrStyled(0, 0, padRightToWidth(title, v.width), titleStyle)
 	}
 
-	lines := v.text.helpLines()
+	lines := v.helpLines()
 	bodyX, bodyY, bodyWidth, bodyHeight := v.contentRect()
 	for y := 0; y < bodyHeight; y++ {
 		screen.PutStrStyled(bodyX, bodyY+y, padRightToWidth("", bodyWidth), bodyStyle)
@@ -24,13 +27,13 @@ func (v *Viewer) drawHelp(screen tcell.Screen) {
 		if lineIndex >= len(lines) {
 			continue
 		}
-		screen.PutStrStyled(bodyX, bodyY+y, truncateToWidth(lines[lineIndex], bodyWidth), bodyStyle)
+		v.drawHelpDocumentLine(screen, bodyX, bodyY+y, bodyWidth, lines[lineIndex])
 	}
 }
 
 func (v *Viewer) maxHelpOffset() int {
 	_, _, _, bodyHeight := v.contentRect()
-	return max(len(v.text.helpLines())-bodyHeight, 0)
+	return max(len(v.helpLines())-bodyHeight, 0)
 }
 
 func (v *Viewer) clampHelpOffset() {
@@ -39,5 +42,27 @@ func (v *Viewer) clampHelpOffset() {
 	}
 	if maxOffset := v.maxHelpOffset(); v.helpOffset > maxOffset {
 		v.helpOffset = maxOffset
+	}
+}
+
+func (v *Viewer) helpLines() []model.Line {
+	doc := model.NewDocumentWithMode(helpDocChunkSize, ansi.RenderPresentation)
+	_ = doc.Append([]byte(v.text.HelpBody))
+	doc.Flush()
+	return doc.Lines()
+}
+
+func (v *Viewer) drawHelpDocumentLine(screen tcell.Screen, x, y, width int, line model.Line) {
+	drawnWidth := 0
+	for _, grapheme := range line.Graphemes {
+		if grapheme.CellWidth <= 0 {
+			continue
+		}
+		if drawnWidth+grapheme.CellWidth > width {
+			break
+		}
+		style := v.toTCellStyle(styleForGrapheme(line, grapheme.RuneStart))
+		screen.PutStrStyled(x+drawnWidth, y, grapheme.Text, style)
+		drawnWidth += grapheme.CellWidth
 	}
 }
